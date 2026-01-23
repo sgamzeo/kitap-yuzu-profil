1fc106e46e0c29eaaa8903bc4840a6862a3e68d4
Gamze <89346098+sgamzeo@users.noreply.github.com>
2026-01-24 02:22:19 +0300

    app page and app route files included

---
+++ lib/core/routes/app_pages.dart
@@ -0,0 +1,23 @@
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/bindings_interface.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:kitap_yuzu_profil/core/routes/app_routes.dart';
+import 'package:kitap_yuzu_profil/feature/pdf_display/add_quotation_view.dart';
+import 'package:kitap_yuzu_profil/feature/pdf_display/pdf_reader.page.dart';
+import 'package:kitap_yuzu_profil/feature/pdf_display/pdf_reader_controller.dart';
+import 'package:kitap_yuzu_profil/feature/pdf_display/pdf_selector_page.dart';
+
+class AppPages {
+  static final pages = [
+    GetPage(
+      name: AppRoutes.home,
+      page: () => const PdfSelectorPage(),
+      binding: BindingsBuilder(() {
+        Get.put(PdfReaderController());
+      }),
+    ),
+    GetPage(name: AppRoutes.reader, page: () => const PdfReaderPage()),
+    GetPage(name: AppRoutes.addQquotation, page: () => AddQuotationView()),
+  ];
+}
---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,5 @@
+class AppRoutes {
+  static const home = '/';
+  static const reader = '/reader';
+  static const addQquotation = '/addQuotation';
+}
